b63d82065351dedf914155ff7a3739416b0532e0
Student <example@example.com>
2019-08-30 08:21:19 -0700

    Delete all eronious material from js css and html

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,4 @@
-// Business Logic for Bank Account
+// Business Logic for
 
-function Account (owner, balance) {
-  this.owner = owner;
-  this.balance = balance;
-}
-
-Account.prototype.deposit = function (amount) {
-  this.balance += amount ;
-}
-
-Account.prototype.withdraw = function (amount) {
-  this.balance -= amount ;
-}
 
 //Front End Logic
-$(document).ready(function(){
-  var newAccount;
-  $('form#new-account').submit(function(event) {
-    event.preventDefault();
-    var owner = $("#new-owner").val();
-    var initDeposit = parseInt($("#initial-deposit").val());
-    $("#balance").html(`${initDeposit}`);
-    $("#new-account").hide();
-    $("#deposit-withdraw").show();
-    $("#balance").show();
-    newAccount = new Account(owner,initDeposit);
-    $('span#alert-owner').text(newAccount.owner);
-    alert("New Account Created : " + newAccount.owner);
-  });
-
-
-
-  $('form#deposit-withdraw').submit(function(event){
-    event.preventDefault();
-    var newDeposit = parseInt($("#deposit").val());
-    var newWithdrawl = parseInt($("#withdraw").val());
-    newAccount.deposit(newDeposit);
-    newAccount.withdraw(newWithdrawl);
-    $("#balance").html("The account balance is " + `${newAccount.balance}`)
-
-  })
-});
