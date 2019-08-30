11cbe1430fa217a76726e276e50ba76895e25dcb
Student <example@example.com>
2019-08-30 10:37:18 -0700

    write prototype to build pizza object and clean up js script

--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,27 @@ var Order = function (totalPrice,pizzas,method) {
   this.method = method;
 };
 
-Order.prototype.compileOrder = function() {
-  this.pizzas.push()//write loop for adding pizzas here.
-};
+
 Order.prototype.calcTotalPrice = function() {
   this.totalPrice = this.pizzas.foreach(function(total){
-    this.pizzas.price += total
-    return(total)
+    this.pizzas.price += total;
+    return(total);
   });
   if (this.method = "Delivery") {
     this.totalPrice + 5
-  }
+  };
   else {
     this.totalPrice + 1
-  }
+  };
 };
 
 Order.prototype.addPizza = function () {
-  this.pizzas.push
-}
+  this.pizzas.push(newPizza);
+};
+
+Order.prototype.addMethod = function(
+  this.method.push(deliveryMethod);
+};
 
 // Business Logic for Pizza
 
@@ -37,20 +39,27 @@ var Pizza = function (price,toppings,size) {
 
 Pizza.prototype.calcPrice = function() {
   if (this.size = "Bagel Bite") {
-  return(10 + (this.toppings.length * .5))
+  return(10 + (this.toppings.length * .5));
   }
   else if (this.size = "Small") {
-    return(15 + (this.toppings.length * .75))
+    return(15 + (this.toppings.length * .75));
   }
   else if (this.size = "Medium") {
-    return(20 + (this.toppings.length *  1))
+    return(20 + (this.toppings.length *  1));
   }
   else if (this.size = "large") {
-    return(25 + (this.toppings.length * 1.5))
+    return(25 + (this.toppings.length * 1.5));
   }
   else if (this.size = "'Murica") {
-    return(30 + (this.toppings.length * 2))
+    return(30 + (this.toppings.length * 2));
   }
+};
+
+
+Pizza.prototype.buildPizza = function() {
+  this.toppings.push(allToppings);
+  this.price.push(pizzaPrice);
+  this.size.push(pizzaSize);
 };
 
 //Front End Logic
