13e9eb326c36d44dc4feef18517ec5eb104c05b0
Student <example@example.com>
2019-08-30 09:46:52 -0700

    Write Pizza and Order Constructors as well as prototypes for both.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
-// Business Logic for
+// Business Logic for order
 
+var Order = function (totalPrice,pizzas,method) {
+  this.totalPrice = totalPrice;
+  this.pizzas = pizzas;
+  this.method = method;
+};
+
+Order.prototype.compileOrder = function() {
+  this.pizzas.push()//write loop for adding pizzas here.
+};
+Order.prototype.calcTotalPrice = function() {
+    this.totalPrice= 
+  }
+
+// Business Logic for Pizza
+
+var Pizza = function (price,toppings,size) {
+  this.price = price;
+  this.toppings = toppings;
+  this.size =  size;
+}
+
+Pizza.prototype.calcPrice =
 
 //Front End Logic
