d5d0eb751e79a4021cb1fa54e91c2a2582024dcc
Student <example@example.com>
2019-08-30 09:54:29 -0700

    Write prototype to calculate pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,8 @@ Order.prototype.compileOrder = function() {
   this.pizzas.push()//write loop for adding pizzas here.
 };
 Order.prototype.calcTotalPrice = function() {
-    this.totalPrice= 
-  }
+    this.totalPrice=
+  };
 
 // Business Logic for Pizza
 
@@ -19,8 +19,24 @@ var Pizza = function (price,toppings,size) {
   this.price = price;
   this.toppings = toppings;
   this.size =  size;
-}
+};
 
-Pizza.prototype.calcPrice =
+Pizza.prototype.calcPrice = function() {
+  if (this.size = "Bagel Bite") {
+  return(10 + (this.toppings.length * .5))
+  }
+  else if (this.size = "Small") {
+    return(15 + (this.toppings.length * .75))
+  }
+  else if (this.size = "Medium") {
+    return(20 + (this.toppings.length *  1))
+  }
+  else if (this.size = "large") {
+    return(25 + (this.toppings.length * 1.5))
+  }
+  else if (this.size = "'Murica") {
+    return(30 + (this.toppings.length * 2))
+  }
+};
 
 //Front End Logic
